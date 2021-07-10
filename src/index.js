236a1f2797c833078730f44ee582c13846533ca5
claudineijrdev <70066437+claudineijrdev@users.noreply.github.com>
2021-07-10 17:27:31 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos?.length >= 10) {
+    return response.status(403).json({ error: "You can register only 10 Todos for free. Be Pro right now!" })
+  } else {
+    return next();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found!" })
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Paramter Id must be a uuid." })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "todo not registered." })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found!" })
+  }
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -67,8 +115,7 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-
-  return response.json(user.todos);
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (request, response) => {
